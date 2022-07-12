68e2da3c8710593d5fecfa5e6b003a6419a3b595
Sam Bazley <sambazley@fastmail.com>
2022-07-12 17:20:59 +0100

    Reformat code

--- main.c
+++ main.c
@@ -25,189 +25,198 @@
 #include <stdio.h>
 #include <string.h>
 
-static PangoFontDescription *fontDesc = 0;
+static PangoFontDescription *font_desc = 0;
 
-struct BlockWidthData {
-    int id;
-    int width;
+struct block_width_data {
+	int id;
+	int width;
 };
 
-struct BlockWidthData *blkData = 0;
-int blkCount = 0;
+struct block_width_data *blk_data = 0;
+int blk_count = 0;
 
 #define S(n, t, d, ...) \
-    .n = { \
-        .name = #n, \
-        .type = t, \
-        .desc = d, \
-        .def.t = __VA_ARGS__, \
-        .val.t = __VA_ARGS__, \
-    },
+	.n = { \
+		.name = #n, \
+		.type = t, \
+		.desc = d, \
+		.def.t = __VA_ARGS__, \
+		.val.t = __VA_ARGS__, \
+	},
 
-struct Settings {
-    struct Setting reset;
+struct settings {
+	struct setting reset;
 } settings = {
-    S(reset, INT, "Resets the width of the given block", -1)
+	S(reset, INT, "Resets the width of the given block", -1)
 };
 
-static void setupFont() {
-    struct BarSettings *barSettings = blockbarGetSettings();
+static void setup_font()
+{
+	struct bar_settings *bar_settings = blockbar_get_settings();
 
-    if (fontDesc) {
-        pango_font_description_free(fontDesc);
-    }
+	if (font_desc) {
+		pango_font_description_free(font_desc);
+	}
 
-    if (barSettings->font.val.STR) {
-        fontDesc = pango_font_description_from_string(barSettings->font.val.STR);
-    }
+	if (bar_settings->font.val.STR) {
+		font_desc = pango_font_description_from_string(bar_settings->font.val.STR);
+	}
 }
 
-int init(struct ModuleData *data) {
-    data->name = "constwidth";
-    data->settings = (struct Setting *) &settings;
-    data->settingCount = 1;
+int init(struct module_data *data)
+{
+	data->name = "constwidth";
+	data->settings = (struct setting *) &settings;
+	data->setting_count = 1;
 
-    setupFont();
+	setup_font();
 
-    return 0;
+	return 0;
 }
 
-void blockAdd(struct Block *blk) {
-    blkData = realloc(blkData, sizeof(struct BlockWidthData) * ++blkCount);
-    blkData[blkCount-1].id = blk->id;
-    blkData[blkCount-1].width = 0;
+void block_add(struct block *blk)
+{
+	blk_data = realloc(blk_data, sizeof(struct block_width_data) * ++blk_count);
+	blk_data[blk_count-1].id = blk->id;
+	blk_data[blk_count-1].width = 0;
 }
 
-void blockRemove(struct Block *blk) {
-    for (int i = 0; i < blkCount; i++) {
-        if (blkData[i].id == blk->id) {
-            blkData[i].id = 0;
-            blkCount--;
-        }
-    }
+void block_remove(struct block *blk)
+{
+	for (int i = 0; i < blk_count; i++) {
+		if (blk_data[i].id == blk->id) {
+			blk_data[i].id = 0;
+			blk_count--;
+		}
+	}
 }
 
-void unload() {
-    if (blkData) {
-        free(blkData);
-    }
+void unload()
+{
+	if (blk_data) {
+		free(blk_data);
+	}
 }
 
-void settingUpdate(struct Setting *setting) {
-    struct BarSettings *barSettings = blockbarGetSettings();
+void setting_update(struct setting *setting)
+{
+	struct bar_settings *bar_settings = blockbar_get_settings();
 
-    if (setting == &(barSettings->font)) {
-        setupFont();
-    } else if (setting == &(settings.reset)) {
-        for (int i = 0; i < blkCount; i++) {
-            if (blkData[i].id == setting->val.INT) {
-                blkData[i].width = 0;
-                break;
-            }
-        }
+	if (setting == &(bar_settings->font)) {
+		setup_font();
+	} else if (setting == &(settings.reset)) {
+		for (int i = 0; i < blk_count; i++) {
+			if (blk_data[i].id == setting->val.INT) {
+				blk_data[i].width = 0;
+				break;
+			}
+		}
 
-        setting->val.INT = -1;
-    }
+		setting->val.INT = -1;
+	}
 }
 
-int exec(struct Block *blk, int bar, struct Click *cd) {
-    (void) blk;
-    (void) bar;
+int exec(struct block *blk, int bar, struct click *cd)
+{
+	(void) blk;
+	(void) bar;
 
-    if (cd) {
-        if (cd->button <= 0) {
-            return 1;
-        }
-    }
+	if (cd) {
+		if (cd->button <= 0) {
+			return 1;
+		}
+	}
 
-    return 0;
+	return 0;
 }
 
-static void drawString(cairo_t *ctx, char *str, color col, int *w) {
-    struct BarSettings *barSettings = blockbarGetSettings();
+static void draw_string(cairo_t *ctx, char *str, color col, int *w)
+{
+	struct bar_settings *bar_settings = blockbar_get_settings();
 
-    PangoLayout *layout = pango_cairo_create_layout(ctx);
-    pango_layout_set_font_description(layout, fontDesc);
-    pango_layout_set_markup(layout, str, -1);
+	PangoLayout *layout = pango_cairo_create_layout(ctx);
+	pango_layout_set_font_description(layout, font_desc);
+	pango_layout_set_markup(layout, str, -1);
 
-    int width, height;
+	int width, height;
 
-    pango_layout_get_pixel_size(layout, &width, &height);
+	pango_layout_get_pixel_size(layout, &width, &height);
 
-    if (*w < width) {
-        *w = width;
-    }
+	if (*w < width) {
+		*w = width;
+	}
 
-    cairo_move_to(ctx, *w / 2 - width / 2, barSettings->height.val.INT / 2 - height / 2);
+	cairo_move_to(ctx, *w / 2 - width / 2, bar_settings->height.val.INT / 2 - height / 2);
 
-    cairo_set_source_rgba(ctx,
-                          col[0] / 255.f,
-                          col[1] / 255.f,
-                          col[2] / 255.f,
-                          col[3] / 255.f);
+	cairo_set_source_rgba(ctx,
+						  col[0] / 255.f,
+						  col[1] / 255.f,
+						  col[2] / 255.f,
+						  col[3] / 255.f);
 
-    pango_cairo_show_layout(ctx, layout);
+	pango_cairo_show_layout(ctx, layout);
 
-    g_object_unref(layout);
+	g_object_unref(layout);
 }
 
-int render(cairo_t *ctx, struct Block *blk, int bar) {
-    struct BarSettings *barSettings = blockbarGetSettings();
+int render(cairo_t *ctx, struct block *blk, int bar)
+{
+	struct bar_settings *bar_settings = blockbar_get_settings();
 
-    char *execdata;
+	char *execdata;
 
-    if (blk->eachmon) {
-        execdata = blk->data[bar].execData;
-    } else {
-        execdata = blk->data->execData;
-    }
+	if (blk->eachmon) {
+		execdata = blk->data[bar].exec_data;
+	} else {
+		execdata = blk->data->exec_data;
+	}
 
-    if (!execdata) {
-        return 0;
-    }
+	if (!execdata) {
+		return 0;
+	}
 
-    struct BlockWidthData *wdata = 0;
+	struct block_width_data *wdata = 0;
 
-    for (int i = 0; i < blkCount; i++) {
-        if (blkData[i].id == blk->id) {
-            wdata = &blkData[i];
-            break;
-        }
-    }
+	for (int i = 0; i < blk_count; i++) {
+		if (blk_data[i].id == blk->id) {
+			wdata = &blk_data[i];
+			break;
+		}
+	}
 
-    if (wdata == 0) {
-        return 0;
-    }
+	if (wdata == 0) {
+		return 0;
+	}
 
-    char *data = malloc(strlen(execdata) + 1);
-    strcpy(data, execdata);
+	char *data = malloc(strlen(execdata) + 1);
+	strcpy(data, execdata);
 
-    color col;
-    memcpy(col, barSettings->foreground.val.COL, sizeof(color));
+	color col;
+	memcpy(col, bar_settings->foreground.val.COL, sizeof(color));
 
-    int len = strlen(data);
+	int len = strlen(data);
 
-    for (int i = 0; i < len; i++) {
-        if (data[i] == '\0') {
-            break;
-        }
+	for (int i = 0; i < len; i++) {
+		if (data[i] == '\0') {
+			break;
+		}
 
-        if (data[i] == '\n') {
-            if (data[i + 1] == '#') {
-                char str [9] = "00000000";
-                int colLen = len - i - 2;
-                if (colLen == 3 || colLen == 4 || colLen == 6 || colLen == 8) {
-                    strncpy(str, data+i+2, colLen+1);
-                    blockbarParseColorString(str, col);
-                }
-            }
-            data[i] = 0;
-        }
-    }
+		if (data[i] == '\n') {
+			if (data[i + 1] == '#') {
+				char str [9] = "00000000";
+				int col_len = len - i - 2;
+				if (col_len == 3 || col_len == 4 || col_len == 6 || col_len == 8) {
+					strncpy(str, data+i+2, col_len+1);
+					blockbar_parse_color_string(str, col);
+				}
+			}
+			data[i] = 0;
+		}
+	}
 
-    drawString(ctx, data, col, &(wdata->width));
+	draw_string(ctx, data, col, &(wdata->width));
 
-    free(data);
+	free(data);
 
-    return wdata->width;
+	return wdata->width;
 }
